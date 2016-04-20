7c495518ab11c521aec2a1815290fc472cd36472
Balamurali Pandranki <balamurali@live.com>
2016-04-21 04:01:09 +0530

    Cleanup unused imports

--- controllers/platforms/telegram.go
+++ controllers/platforms/telegram.go
@@ -16,33 +16,29 @@ func TelegramBot(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	apiToken := "213239467:AAGWDAvFMfdfXuwlMkC2dSwKWEaW-NVl4bo"
 
-	if err := json.NewDecoder(r.Body).Decode(update); err != nil {
-		return  err
-	}
+	json.NewDecoder(r.Body).Decode(update) //Decode incoming json
 
 	switch update.Type() {
 	case telegram.MessageUpdate:
 		msg := update.Message
+		/*
 		typ := msg.Type()
 		if typ == telegram.TextMessage {
-			fmt.Printf("<-%d, From:\t%s, Text: %s \n", msg.ID, msg.Chat, *msg.Text)
+			//fmt.Printf("<-%d, From:\t%s, Text: %s \n", msg.ID, msg.Chat, *msg.Text)
 
-		}
-
+		}*/
 		api, err := telegram.New(apiToken)
-
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		/*
 		// just to show its working
-		fmt.Printf("User ID: %d\n", api.ID)
-		fmt.Printf("Bot Name: %s\n", api.Name)
-		fmt.Printf("Bot Username: %s\n", api.Username)
-
+		//fmt.Printf("User ID: %d\n", api.ID)
+		//fmt.Printf("Bot Name: %s\n", api.Name)
+		//fmt.Printf("Bot Username: %s\n", api.Username)
+		*/
 		// now simply echo that back
 		outMsg, err := api.NewOutgoingMessage(telegram.NewRecipientFromChat(msg.Chat), *msg.Text).Send()
-
 		if err != nil {
 			fmt.Printf("Error sending: %s\n", err)
 		}
