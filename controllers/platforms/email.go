933b7e76a29cc4e118a85aac17527f8ce48f8594
Balamurali Pandranki <balamurali@live.com>
2016-06-19 19:45:12 +0530

    Mailgun parsing webhook

--- controllers/platforms/email.go
+++ controllers/platforms/email.go
@@ -6,11 +6,22 @@ import (
 	//"github.com/hectane/hectane/email"
 	"net/smtp"
 	"log"
+	"fmt"
+	"encoding/base64"
+	"strings"
+	"net/mail"
 )
+
+func encodeRFC2047(String string) string{
+	// use mail's rfc2047 to encode any string
+	addr := mail.Address{String, ""}
+	return strings.Trim(addr.String(), " <>")
+}
 
 func EmailBot(c *gin.Context) {
 	sender,_ := c.GetPostForm("sender")
 	from,_ := c.GetPostForm("from")
+	subject,_ := c.GetPostForm("subject")
 
 	// Set up authentication information.
 	auth := smtp.PlainAuth(
@@ -19,6 +30,23 @@ func EmailBot(c *gin.Context) {
 		"915dc442862acfda08672c116d117be2",
 		"smtp.mailgun.org",
 	)
+
+	header := make(map[string]string)
+	header["From"] = from
+	header["To"] = sender
+	header["Subject"] = encodeRFC2047("RE: "+subject)
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/html; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	body := "<html><body> Hi "+ from +", <br> Thanks for mailing me. <br> Currently i am still learning to read and replying to your emails <br> Regards, Yours Asha ;) </body></html>"
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
 	merr := smtp.SendMail(
@@ -26,8 +54,7 @@ func EmailBot(c *gin.Context) {
 		auth,
 		"asha@heyasha.com",
 		[]string{sender},
-		[]byte("<html><body> Hi "+ from +", <br> Thanks for mailing me. <br> Currently i am still learning to read and replying to your emails <br> Regards, Yours Asha ;) </body></html>"),
-	)
+		[]byte(message),)
 	if merr != nil {
 		log.Fatal(merr)
 	}
