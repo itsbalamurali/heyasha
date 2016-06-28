2b25c76f4f3bde24e04f814bc907b4f167056f3e
Balamurali Pandranki <balamurali@live.com>
2016-06-28 05:52:45 +0530

    Clean packages

--- vendor/github.com/hectane/hectane/queue/config.go
+++
@@ -1,7 +0,0 @@
-package queue
-
-// Application configuration.
-type Config struct {
-	Directory              string `json:"directory"`
-	DisableSSLVerification bool   `json:"disable-ssl-verification"`
-}
